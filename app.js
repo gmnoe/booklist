b4bc3b6a6fe1809505d58fee588417a46984ded6
Greg Noethlich <gmnoethlich@gmail.com>
2021-08-05 16:10:10 -0500

    Validation & Alert

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ UI.prototype.addBookToList = function (book) {
     `;
     list.appendChild(row);
 }
+// Show alert
+UI.prototype.showAlert = function(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add class name
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout after 3 seconds
+    setTimeout(function () {
+        document.querySelector('.alert').remove();}, 3000);
+    }
+
 // Clear Fields
 UI.prototype.clearFields = function () {
     document.querySelector('#title').value = '';
@@ -38,10 +57,17 @@ document.querySelector('#book-form').addEventListener('submit', function(e) {
     const book = new Book(title, author, isbn);
     // Instantiate UI
     const ui = new UI();
-    // Add book to list
-    ui.addBookToList(book);
-    // Clear fields
-    ui.clearFields();
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+        // Show success
+        ui.showAlert('Book Added!', 'success')
+        // Clear fields
+        ui.clearFields();
+    }
     
     e.preventDefault();
 })
